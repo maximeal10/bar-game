e8fc4fd4ee7699dee1cd7125d4545ed9224fcbeb
Maxime Ashurov <maximeal10@gmail.com>
2024-08-26 20:05:52 +0200

    try to fix orientation

--- sources/bar/Config.js
+++ sources/bar/Config.js
@@ -1,8 +1,8 @@
 import {Vector2D} from "../Vector2D.js"
 
 export const config = {
-    padding: new Vector2D(0.1, 0.05),
-    playerSizeX: 0.1,
+    padding: new Vector2D(0.2, 0.12),
+    playerSizeX: 0.15,
     itemSizeX: 0.05,
     positinCount: 4,
     itemLifeTime: 1500
